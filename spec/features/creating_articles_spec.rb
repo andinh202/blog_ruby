bf2ec14ac790031a229bbaae46e6cdec6e0e3b2b
ANDINH <thaiantamk@gmail.com>
2017-11-24 05:04:53 +0000

    Setup bootstarp

---
+++ spec/features/creating_articles_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature "Creating Articles" do
+    scenario "A user create a new articles" do
+    visit "/"
+    click_link "new Articles"
+    fill_in "Title" ,with: "creating the first article"
+    fill_in "Body" , with: "Lorem Isump"
+    click_button "Create Article"
+    expect(page).to have_content("Article has been created")
+    expect(page current_path).to eq(articles_path)
+    
+    
+    
+    end
+    
+end
+
