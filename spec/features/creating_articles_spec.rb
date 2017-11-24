1b3d9ca4233a1a1985199db07023425b191f55d1
ANDINH <thaiantamk@gmail.com>
2017-11-24 08:48:32 +0000

    Create Article Successfully

--- spec/features/creating_articles_spec.rb
+++ spec/features/creating_articles_spec.rb
@@ -8,8 +8,8 @@ RSpec.feature "Creating Articles" do
     fill_in "Body" , with: "Lorem Isump"
     click_button "Create Article"
     expect(page).to have_content("Article has been created")
-    expect(page current_path).to eq(articles_path)
-    
+    expect(page).to eq(new_articles_path)
+
     
     
     end
